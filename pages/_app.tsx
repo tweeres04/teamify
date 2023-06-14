86f47b7f9a38eaaa905428ec45a22625e07ac3d2
Tyler Weeres <tweeres04@gmail.com>
2023-06-14 09:03:58 -0700

    Add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,21 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<link rel="shortcut icon" href="/team.png" />
 				<meta name="theme-color" content="#6ABBEB" />
 				<link rel="manifest" href="/manifest.json" />
+				<script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-JB7FG6K9B2"
+				></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+
+							gtag('config', 'G-JB7FG6K9B2');
+						`,
+					}}
+				></script>
 			</Head>
 			<Component {...pageProps} />
 		</>
